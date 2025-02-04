55c3b8764f58ed7800cd1b9adac20e035db266d4
BitBlasters <scottmitchell@mac.com>
2025-02-04 08:16:30 -0700

    Made some changes to tht code to put code in functions, added commnets

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
-// Prompt the user for a list of integers separated by commas.
+//
+// script.js
+// Created by Scott Mitchell on 01/30/2025
+// Block 15 - Froyo Orders
+//
+
+// take the array of flavors and create the order
+function createOrder(flavors) {
+  const froyo_order = {};
+
+  // loop through the flavors and add them to the order
+  for (let i = 0; i < flavors.length; i++) {
+    // checkk if the flavor is already in the order
+    if (froyo_order[flavors[i]]) {
+      froyo_order[flavors[i]] += 1;
+    } else {
+      // not already in the order so add it
+      froyo_order[flavors[i]] = 1;
+    }
+  }
+  return froyo_order;
+}
+
+function convertUserInputIntoArray(userInputString) {
+  // Split the string of strings into an array of strings.
+  const stringArray = userInputString.split(",");
+
+  // Convert the array of strings into an array of strings.
+  const flavors = [];
+  for (let i = 0; i < stringArray.length; i++) {
+    // trim the white space from the beginning and end of the string.
+    const flavor = stringArray[i].trim();
+    // oush into the array
+    flavors.push(flavor);
+  }
+  return flavors;
+}
+
+// Prompt the user for a list of stringss separated by commas.
 const userInputString = prompt(
   "Please enter some froyo flavors you would like to order (please seperate each flavor with a comma):"
 );
-
-// Split the string of numbers into an array of strings.
-const stringArray = userInputString.split(",");
-
-// Convert the array of strings into an array of numbers.
-const flavors = [];
-for (let i = 0; i < stringArray.length; i++) {
-  const flavor = stringArray[i].trim();
-  flavors.push(flavor);
-}
+// convert the user eneterd sring into an array of flavors ordered
+flavors = convertUserInputIntoArray(userInputString);
 
 console.log(flavors);
-
-const froyo_order = {};
-
-for (let i = 0; i < flavors.length; i++) {
-  if (froyo_order[flavors[i]]) {
-    froyo_order[flavors[i]] += 1;
-  } else {
-    froyo_order[flavors[i]] = 1;
-  }
-}
+// create the order
+froyo_order = createOrder(flavors);
 
 console.log(froyo_order);
